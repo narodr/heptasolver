3a573881461eaaf3aa26c477bc8d88035997f68d
narodr <narodriguez@gradiant.org>
2025-07-08 09:25:56 +0200

    remove unused utils

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,7 @@
 import time
 
 
-def escribir_palabras(palabras, file_name):
-    with open(file_name, "w") as file:
-        for palabra in palabras:
-            file.write(palabra + "\n")
-
-def countdown(seconds):
-    for i in range(seconds, 0, -1):
+def countdown(segundos):
+    for i in range(segundos, 0, -1):
         print(f"\rEmpezando en {i}...", end="", flush=True)
         time.sleep(1)
