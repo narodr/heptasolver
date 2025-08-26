3483b57216a490cba5ee2f49c0b5a50ffee8ebcf
narodr <narodriguez@gradiant.org>
2025-08-26 12:15:43 +0200

    fix: random letters when using `-l`

--- src/heptasolver/application/play.py
+++ src/heptasolver/application/play.py
@@ -18,21 +18,29 @@ from heptasolver.infrastructure import utils
     help='La letra central del heptagrama (p.e. --centro a)'
 )
 def play(letras, centro):
-    if centro and letras:
+    playable = False
+    if (letras and centro):
         solver = Solver(letras=letras, centro=centro)
         if not solver.is_playable():
             print("Not playable. Try another combination of letters and center")
             sys.exit()
-
-    if not centro:
-        centro = utils.get_random_center(letras)
-
-    playable = False
-    while not playable:
-        # TODO: Revisar esto porque es una mierda
-        letras = utils.get_random_letters(centro)
-        solver = Solver(letras=letras, centro=centro)
-        playable = solver.is_playable()
+    elif (letras and not centro):
+        while not playable:
+            for letra in letras:
+                centro = letra
+                solver = Solver(letras=letras, centro=centro)
+                playable = solver.is_playable()
+    elif (not letras and centro):
+        while not playable:
+            letras = utils.get_random_letters(centro)
+            solver = Solver(letras=letras, centro=centro)
+            playable = solver.is_playable()
+    else:
+        while not playable:
+            centro = utils.get_random_center()
+            letras = utils.get_random_letters(centro)
+            solver = Solver(letras=letras, centro=centro)
+            playable = solver.is_playable()
 
     curses.wrapper(tui.main, solver)
 
